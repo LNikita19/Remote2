0d768bbdda17db715633a73bc76b4e3d7205dbb0
LNikita19 <nikita.lilhore@pinnacle.in>
2025-09-03 11:15:53 +0530

    ++package

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,41 @@
+// const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+
+// module.exports = {
+//   webpack(config: any) {
+//     config.plugins.push(
+//       new NextFederationPlugin({
+//         name: "remote2",
+//         filename: "static/chunks/remoteEntry.js",
+//         exposes: {
+//           "./RemotePage": "./src/pages/index.tsx",
+//         },
+//         shared: {
+//           react: { singleton: true, requiredVersion: false, eager: true },
+//           "react-dom": { singleton: true, requiredVersion: false, eager: true },
+//         },
+//       })
+//     );
+//     return config;
+//   },
+
+
+//   experimental: {
+//     esmExternals: false,
+//   },
+
+//   output: "standalone",
+// };
+
+// @ts-check
+
 const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
 
-module.exports = {
-  webpack(config: any) {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Required when deploying to Vercel with module federation
+  output: "standalone",
+
+  webpack(config: { plugins: any[]; }, { isServer }: any) {
     config.plugins.push(
       new NextFederationPlugin({
         name: "remote2",
@@ -15,14 +49,14 @@ module.exports = {
         },
       })
     );
+
     return config;
   },
 
-
+  // Extra stability (avoid esm externals issues in some Next.js 15 setups)
   experimental: {
     esmExternals: false,
   },
-
-  output: "standalone",
 };
 
+module.exports = nextConfig;
